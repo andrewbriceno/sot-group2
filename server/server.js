b8004c8ea07d95c8d9c8f494079191f9d29910c9
vinny-martinson <vinicius.martins@ufl.edu>
2020-02-24 15:56:20 -0500

    clean up

--- server/server.js
+++ server/server.js
@@ -30,35 +30,10 @@ app.use(bodyParser.json());
 app.use('/', express.static('./../../client'));
 app.use(express.static('client'))
 
-
-/* The next three middleware are important to the API that we are building */
-
-/* Request Handler for route /api/listings
-   TODO: Update the code to meet the required format - app.use('/api/listings', appropriateMiddleWare)
-   use the listings router middleware for requests to the api
-   check the variables list above
-*/
-app.use('/api/listings/', listingsRouter);
 app.use('/api/users/', usersRouter);
 
-
-/* Request Handler for coordinates
-   This is a server wrapper around Open Cage Data Geocoding API to get latitude + longitude coordinates from address */
-app.post('/api/coordinates', getCoordinates, (req, res) => {
-    res.send(req.results);
-});
-
-
-/* Request handler for all other routes
-   Sends a response (res) to go to the homepage for all routes not specified */
 app.all('/*', (req, res) => {
 
-    /*Add YOUR CODE HERE
-       see https://expressjs.com/en/api.html#res.sendFile
-       see https://nodejs.org/api/path.html
-       The path.resolve() method returns a string and resolves a sequence of paths or path segments into an absolute path.
-       If no path segments are passed, path.resolve() will return the absolute path of the current working directory.
-    */
     res.sendFile(path.resolve("client/index.html"));
 });
 const PORT = process.env.PORT || 3000;
