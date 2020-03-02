b82d6931cd7e6fc6245fc3955d31f4629cabe66b
Cole Frishman <awesomeeloc@gmail.com>
2020-03-02 12:30:19 -0500

    created react app in client folder

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './considerherbs2500x579.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          considerherbs.com
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
