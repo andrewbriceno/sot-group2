aa0f101c532ce9be7eabce67f873c61c164ae3d7
Cole Frishman <awesomeeloc@gmail.com>
2020-03-07 22:26:30 -0500

    updates based on new pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from "./components/Home"
 import About from "./components/AboutPage"
+import DYK from "./components/DidYouKnow"
+import Forum from "./components/Forum"
+import Schedule from "./components/SchedulePage"
+import Glossary from "./components/Glossary"
 import Login from "./components/LoginPage"
 import Admin from "./components/AdminPanel"
 import './App.css';
@@ -15,6 +19,10 @@ function App() {
                     <Route path="/" component={Home} exact/>
                     <Route path="/home" component={Home}/>
                     <Route path="/about" component={About}/>
+                    <Route path="/didyouknow" component={DYK}/>
+                    <Route path="/forum" component={Forum}/>
+                    <Route path="/schedule" component={Schedule}/>
+                    <Route path="/glossary" component={Glossary}/>
                     <Route path="/login" component={Login}/>
                     <Route path="/admin" component={Admin}/>
                     <Route path="/admingloss" component={AdminGlossary}/>
