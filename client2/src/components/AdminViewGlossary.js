68d2e3e432a26d13c001991b1dc7d92cc951ce85
Jerdan1980 <jeremytinana@gmail.com>
2020-03-23 02:08:55 -0400

    Remove extra indent

--- client2/src/components/AdminViewGlossary.js
+++ client2/src/components/AdminViewGlossary.js
@@ -8,50 +8,49 @@ export default class AdminViewGlossary extends React.Component {
       glossary: []
     }
 
-    componentDidMount() {
-      axios.get(`http://localhost:${config.server_port}/api/admin/get_glossary`)
-        .then(res => {
-          const glossary = res.data;
-          this.setState({ glossary });
-        })
-    }
+  componentDidMount() {
+    axios.get(`http://localhost:${config.server_port}/api/admin/get_glossary`)
+      .then(res => {
+        const glossary = res.data;
+        this.setState({ glossary });
+      })
+  }
 
-    deleteGloss(e) {
-      const title = e.target.value;
-      axios.delete(`http://localhost:${config.server_port}/api/admin/delete_glossary/${title}`)
-        .then(res => {
-          console.log(`Deleted ${res.data.title}!`)
-        });
-      axios.get(`http://localhost:${config.server_port}/api/admin/get_glossary`)
-        .then(res => {
-          const glossary = res.data;
-          this.setState({ glossary });
-        });
-    }
+  deleteGloss(e) {
+    const title = e.target.value;
+    axios.delete(`http://localhost:${config.server_port}/api/admin/delete_glossary/${title}`)
+      .then(res => {
+        console.log(`Deleted ${res.data.title}!`)
+      });
+    axios.get(`http://localhost:${config.server_port}/api/admin/get_glossary`)
+      .then(res => {
+        const glossary = res.data;
+        this.setState({ glossary });
+      });
+  }
 
-    render() {
-      return (
-        <div>
-          <tr>
-            <td>Title</td>
-            <td>Usage</td>
-          </tr>
-            {
-              this.state.glossary.filter(glossary => glossary.title.toLowerCase().includes(this.props.query.toLowerCase())).map(glossary => {
-                return(
-                  <tr key={glossary.id} name={glossary.title}>
-                    <td>{glossary.title}</td>
-                    <td>{glossary.usage}</td>
-                    <a class="btn btn-primary text-white px-4" href={`/admin/edit_glossary?key=${glossary._id}`}>EDIT</a>
-                    {/*https://stackoverflow.com/questions/34875557/creating-custom-function-in-react-component*/}
-                    <button class="btn btn-primary text-white px-4" onClick={this.deleteGloss.bind(this)} value={glossary.title}>DELETE</button>
-                  </tr>
-                )
-              })
-            }
-        </div>
-      )
-    }
-
+  render() {
+    return (
+      <div>
+        <tr>
+          <td>Title</td>
+          <td>Usage</td>
+        </tr>
+          {
+            this.state.glossary.filter(glossary => glossary.title.toLowerCase().includes(this.props.query.toLowerCase())).map(glossary => {
+              return(
+                <tr key={glossary.id} name={glossary.title}>
+                  <td>{glossary.title}</td>
+                  <td>{glossary.usage}</td>
+                  <a class="btn btn-primary text-white px-4" href={`/admin/edit_glossary?key=${glossary._id}`}>EDIT</a>
+                  {/*https://stackoverflow.com/questions/34875557/creating-custom-function-in-react-component*/}
+                  <button class="btn btn-primary text-white px-4" onClick={this.deleteGloss.bind(this)} value={glossary.title}>DELETE</button>
+                </tr>
+              )
+            })
+          }
+      </div>
+    )
+  }
 };
 // export default ViewProducts;
