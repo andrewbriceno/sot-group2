fb768c64c889882a0ef4de71ffd5270b03a0a6ea
vinny-martinson <vinicius.martins@ufl.edu>
2020-03-25 13:52:00 -0400

    token auth done

--- client2/src/components/LoginButton.js
+++ client2/src/components/LoginButton.js
@@ -1,5 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import config from './config.js';
 import React, {useState, useEffect} from 'react';
 
 const LoginButton = () => {
@@ -8,30 +11,69 @@ const LoginButton = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    let data = {
+      "email": email,
+      "password": pw
+    }
+
+    axios.post(`http://localhost:${config.server_port}/api/users/signin`, data)
+      .then(res => {
+        const token = res.data.token;
+        localStorage.setItem(`user-token-${email}`, token);
+        console.log("token => ", token);
+      })
+
+  };
+
   // render() {
-    return (
-      <>
-      <Button variant="primary" onClick={handleShow}>
-        Login
-      </Button>
+  return (
+    <>
+    <Button variant="primary" onClick={handleShow}>
+      Login
+    </Button>
+    <Form>
 
       <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Let's get the extra credit</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>Put login here</Modal.Body>
-      <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose}>
-        Close
-      </Button>
-      <Button variant="primary" onClick={handleClose}>
-        Let's go!
-      </Button>
-      </Modal.Footer>
+        <Modal.Header closeButton>
+          <Modal.Title>Log in</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" id="emailinput" onChange = {(event) => setEmail(event.target.value)} />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" id="pwinput" onChange = {(event) => setPw(event.target.value)} />
+          </Form.Group>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Login
+          </Button>
+
+        </Modal.Footer>
       </Modal>
-      </>
-    );
-  // }
+    </Form>
+
+    </>
+);
+// }
 }
 
 export default LoginButton;
