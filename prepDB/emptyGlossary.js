c4036cc7da2e0b67e9f5786951c45e001fb91e02
Andrew Briceno <andrewbriceno2000@gmail.com>
2020-03-30 18:40:58 -0400

    testing

--- prepDB/emptyGlossary.js
+++ prepDB/emptyGlossary.js
@@ -1,1 +1,9 @@
+import mongoose from 'mongoose';
+import GlossaryModel from '../models/GlossaryModel.js';
+import config from '../config/config.js';
+
+mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true});
+GlossaryModel.deleteMany({}, (err) => {
+    if (err) throw err;
+});
 console.log("emptied Glossary");
