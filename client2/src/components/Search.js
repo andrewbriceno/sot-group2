1586867b4aa0242475a80397bc64e2152d769ef3
Cole Frishman <awesomeeloc@gmail.com>
2020-03-29 22:09:49 -0400

    search bar redirects to searched item on submit on product page

--- client2/src/components/Search.js
+++ client2/src/components/Search.js
@@ -1,14 +1,27 @@
 import React, {useState, useEffect} from 'react';
 
 const SearchField = () => {
+    const [searchText, setSearchText] = useState("");
+
     let url = window.location.href
     url = url.split("/")[3]
 
+    const goToPage = (value) => {
+        alert(value);
+        if(value){
+            document.location = "/" + url + "/" + value;
+        }
+    }
 
     if (url == "products") {
       return (
-          <form>
-              <input style={{borderRadius: '30px', boxShadow: '2px 2px'}} type="text" placeholder="Search for any products"/>
+          <form  target="_blank" onSubmit={() => goToPage(searchText)}>
+              <input 
+                style={{borderRadius: '30px', boxShadow: '2px 2px'}}
+                type="text"
+                placeholder="Search for any products"
+                value = {searchText}
+                onChange={(val) => {setSearchText(val.target.value)}}/>
           </form>
       );
     } else {
